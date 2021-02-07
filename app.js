a9a7779a973385e7422b06251ac7a1d9f2c10f6b
Kazi Reazul Islam <kazireazul1999@gmail.com>
2021-02-07 20:52:21 +0600

    Updated-All-Thing

--- app.js
+++ app.js
@@ -1,37 +1,69 @@
-const inputValue = document.getElementById("inputValue").value;
+const inputValue = document.getElementById("input-value");
 const searchBtn = document.getElementById("searchBtn");
-searchBtn.addEventListener("click", function () {
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputValue}`)
-    .then((res) => res.json())
-    .then((data) => displayMeals(data.meals));
+const menuItemsDiv = document.getElementById("menu-items");
+const menuDetails = document.getElementById("menu-items-details");
+const closeBtn = document.getElementById("cross");
 
-  const displayMeals = (meals) => {
-    const menueItemsDiv = document.getElementById("menue-items");
-
-    meals.forEach((meal) => {
-      const itemDiv = document.createElement("div");
-      itemDiv.className = "item";
-      const items = `
-        <img src="${meal.strMealThumb}" width="300px" >
-          <h3 class="items-heading">${meal.strMeal}</h3>
-      `;
-      itemDiv.innerHTML = items;
-
-      menueItemsDiv.appendChild(itemDiv);
-    });
-
-    // for (let i = 0; i < meals.length; i++) {
-    //   const meal = meals[i];
-
-    //   const itemDiv = document.createElement("div");
-    //   itemDiv.className = "item";
-    //   const items = `
-    //   <img src="${meal.strMealThumb}" width="300px" >
-    //     <h3 class="items-heading">${meal.strMeal}</h3>
-    // `;
-    //   itemDiv.innerHTML = items;
-
-    //   menueItemsDiv.appendChild(itemDiv);
-    // }
-  };
+closeBtn.addEventListener("click", function () {
+  menuDetails.style.display = "none";
 });
+
+searchBtn.addEventListener("click", function () {
+  const input = inputValue.value;
+
+  displayFoodItems(input);
+});
+
+function displayFoodItems(input) {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      singleItem(data);
+    });
+}
+
+function singleItem(item) {
+  menuItemsDiv.innerHTML = "";
+
+  const getItems = item.meals;
+  getItems.forEach((meal) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.className = "item";
+    const items = `
+            
+            <img src="${meal.strMealThumb}" width="300px" >
+              <h3 class="items-heading">${meal.strMeal}</h3>
+          `;
+    itemDiv.innerHTML = items;
+    menuItemsDiv.appendChild(itemDiv);
+
+    itemDiv.addEventListener("click", function () {
+      menuDetails.style.display = "flex";
+      itemsDetails(meal);
+    });
+  });
+}
+
+function itemsDetails(meal) {
+  const detailsBody = document.querySelector(".details");
+  detailsBody.innerHTML = `
+  <img
+  src="${meal.strMealThumb}"
+  alt=""
+/>
+<div class="details-bottom">
+  <h1>${meal.strMeal}</h1>
+  <h3>Ingredients</h3>
+
+  <ul>
+    <li><i class="fas fa-check-circle"></i>${meal.strIngredient1}</li>
+    <li><i class="fas fa-check-circle"></i>${meal.strIngredient2}</li>
+    <li><i class="fas fa-check-circle"></i>${meal.strIngredient3}</li>
+    <li><i class="fas fa-check-circle"></i>${meal.strIngredient4}</li>
+    <li><i class="fas fa-check-circle"></i>${meal.strIngredient5}</li>
+  </ul>
+</div>
+  
+  `;
+}
